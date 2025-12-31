e14356e5f631989993caae8f418571245e1bc894
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2025-12-30 23:16:24 -0500

    import libraries
    import your steam and/or CLZ library

--- GameCollectionManager.Shared/Models/CLZ_Model.cs
+++ GameCollectionManager.Shared/Models/CLZ_Model.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace GameCollectionManager.Shared.Models
 {
-    internal class CLZ_Model
+    public class CLZ_Model
     {
+        public string Platform { get; set; }
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public string Genre { get; set; }
+        public string AddedDate { get; set; }
     }
 }
